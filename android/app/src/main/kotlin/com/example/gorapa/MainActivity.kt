b697e012de81f9fd328cdf9abc08dce23cd573a4
egg-z1 <kimfreedom25@gmail.com>
2023-10-09 12:18:04 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/gorapa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gorapa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,245 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Charactor card',
+      home: Mycard(),
+    );
+  }
+}
+
+class Mycard extends StatelessWidget {
+  const Mycard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'POKEMON',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 174, 0),
+        elevation: 0.0,
+        // leading: IconButton(
+        //   icon: const Icon(Icons.menu),
+        //   onPressed: () {
+        //     debugPrint('menu is clicked');
+        //   },
+        // ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                debugPrint('shopping cart is clicked');
+              },
+              icon: const Icon(Icons.shopping_cart)),
+          IconButton(
+              onPressed: () {
+                debugPrint('search is clicked');
+              },
+              icon: const Icon(Icons.search))
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('assets/gorapa.png'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/gorapa.png'),
+                  backgroundColor: Colors.amber,
+                )
+              ],
+              accountName: const Text('Psyduck'),
+              accountEmail: const Text('psyduck@pokemon.com'),
+              onDetailsPressed: () {
+                debugPrint('detail click');
+              },
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 255, 213, 122),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40.0),
+                      bottomRight: Radius.circular(40.0))),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.home,
+                color: Colors.amber,
+              ),
+              title: const Text('home'),
+              onTap: () {
+                debugPrint('Home is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.amber,
+              ),
+              title: const Text('setting'),
+              onTap: () {
+                debugPrint('setting is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.question_answer,
+                color: Colors.amber,
+              ),
+              title: const Text('Q&A'),
+              onTap: () {
+                debugPrint('Q&A is clicked');
+              },
+              trailing: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/crying.gif'),
+                  radius: 60.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Color.fromARGB(255, 255, 174, 0),
+                thickness: 0.8,
+                endIndent: 30.0,
+              ),
+              Text(
+                'Name',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 174, 0),
+                    letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 6.0,
+              ),
+              Text(
+                'Psyduck',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 174, 0),
+                    fontSize: 28.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text('pokemon power level',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 255, 174, 0),
+                      letterSpacing: 2.0)),
+              SizedBox(
+                height: 6.0,
+              ),
+              Text(
+                '486',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 255, 174, 0),
+                    fontSize: 28.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.stars, color: Color.fromARGB(255, 255, 174, 0)),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'headache',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 255, 174, 0),
+                        fontSize: 16.0,
+                        letterSpacing: 1.0),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.mood_bad, color: Color.fromARGB(255, 255, 174, 0)),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'crying',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 255, 174, 0),
+                        fontSize: 16.0,
+                        letterSpacing: 1.0),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.sentiment_very_satisfied,
+                      color: Color.fromARGB(255, 255, 174, 0)),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'smiling',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 255, 174, 0),
+                        fontSize: 16.0,
+                        letterSpacing: 1.0),
+                  ),
+                ],
+              ),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/gorapa.png'),
+                  radius: 40.0,
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text("항상 두통에 시달리고 있다.")));
+        },
+        style: const ButtonStyle(
+            textStyle: MaterialStatePropertyAll(
+                TextStyle(fontSize: 15, color: Colors.white)),
+            backgroundColor: MaterialStatePropertyAll(Colors.amber)),
+        child: const Text(
+          "Psyduck's state",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
